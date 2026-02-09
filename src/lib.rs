68b0764156e4cb0cf4806c09c8b2d6b0ded0d7f0
aptxsblj4869-coder <aptxsblj4869@gmail.com>
2026-02-09 21:21:58 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use fastedge::http::{Request, Response, Body};
 
-#[fastedge::http_handler]
-pub fn main(req: Request<Body>) -> std::result::Result<Response<Body>, Box<dyn std::error::Error>> {
-    // 检查是否为 WebSocket
-    if let Some(v) = req.headers().get("upgrade") {
-        if v == "websocket" {
+// 放弃使用报错的 #[fastedge::http_handler] 宏，改用底层导出方式
+#[no_mangle]
+pub fn main(req: Request<Body>) -> Result<Response<Body>, Box<dyn std::error::Error>> {
+    // 处理 WebSocket 握手
+    if let Some(upgrade) = req.headers().get("upgrade") {
+        if upgrade == "websocket" {
             return Ok(Response::builder()
                 .status(101)
                 .header("Connection", "Upgrade")
@@ -13,6 +14,7 @@ pub fn main(req: Request<Body>) -> std::result::Result<Response<Body>, Box<dyn s
         }
     }
 
+    // 默认返回
     Ok(Response::builder()
         .status(200)
         .body(Body::from("Gcore Node Active"))?)
